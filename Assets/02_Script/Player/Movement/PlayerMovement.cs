d786c803507226d2db0aacdd9f9a9475dc32cec5
dkdkdsa <bos94330@gmail.com>
2023-05-27 21:54:52 +0900

    PlayerMovement

---
+++ Assets/02_Script/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : PlayerBehaviorRoot
+{
+
+
+
+    public override void AddEvent()
+    {
+
+
+
+    }
+
+    public override void RemoveEvent()
+    {
+
+
+
+    }
+}
--- Assets/02_Script/Player/System/Root/PlayerBehaviorRoot.cs
+++ Assets/02_Script/Player/System/Root/PlayerBehaviorRoot.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public abstract class PlayerBehaviorRoot : PlayerRoot, IEventObject
 {
 
+    protected PlayerEventSystem eventSystem;
+
     protected override void Awake()
     {
 
         base.Awake();
+
+        eventSystem = GetComponent<PlayerEventSystem>();
 
         AddEvent();
 
