ee55a336e0f703df3746fbf7d21ba71a37346074
dkdkdsa <bos94330@gmail.com>
2023-05-27 21:49:43 +0900

    PlayerEventSystem

--- Assets/02_Script/Player/System/PlayerEventSystem.cs
+++ Assets/02_Script/Player/System/PlayerEventSystem.cs
@@ -10,11 +10,25 @@ public class PlayerEventSystem : MonoBehaviour
     public event Action<float> OnVerticalAxisEvent;
     public event Action OnJumpKeyPressEvent;
 
+    private void Update()
+    {
+        
+        JumpEventExecute();
+
+    }
+
     private void FixedUpdate()
     {
 
         OnHorizontalAxisEvent?.Invoke(Input.GetAxisRaw("Horizontal"));
         OnHorizontalAxisEvent?.Invoke(Input.GetAxisRaw("Vertical"));
+
+    }
+
+    private void JumpEventExecute()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space)) OnJumpKeyPressEvent?.Invoke();
 
     }
 
