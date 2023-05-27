940c5ed1bae9ed247f7291907178111d564ae957
dkdkdsa <bos94330@gmail.com>
2023-05-27 21:57:14 +0900

    PlayerMovement

--- Assets/02_Script/Player/Movement/PlayerMovement.cs
+++ Assets/02_Script/Player/Movement/PlayerMovement.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerMovement : PlayerBehaviorRoot
 {
 
+    private float horInput, verInput;
 
+    private void SetHor(float v) { horInput = v; }
+    private void SetVer(float v) { verInput = v; }
 
     public override void AddEvent()
     {
 
-
+        eventSystem.OnHorizontalAxisEvent += SetHor;
+        eventSystem.OnVerticalAxisEvent += SetVer;
 
     }
 
     public override void RemoveEvent()
     {
 
-
+        eventSystem.OnHorizontalAxisEvent -= SetHor;
+        eventSystem.OnVerticalAxisEvent -= SetVer;
 
     }
 }
