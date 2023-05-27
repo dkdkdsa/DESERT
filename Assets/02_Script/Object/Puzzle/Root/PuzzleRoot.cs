801ddd69fb23ac1f9178330375d72a135902b106
dkdkdsa <bos94330@gmail.com>
2023-05-28 01:09:56 +0900

    PuzzleObjectRoot

--- Assets/02_Script/Object/Puzzle/Root/PuzzleRoot.cs
+++ Assets/02_Script/Object/Puzzle/Root/PuzzleRoot.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleRoot : MonoBehaviour
+public abstract class PuzzleRoot : MonoBehaviour
 {
-    
 
+    public abstract void PuzzleClearEvent();
 
 }
