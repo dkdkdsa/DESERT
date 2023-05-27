be60e88b4b0b9daa8f59fe4cab9fec8049ee34e1
dkdkdsa <bos94330@gmail.com>
2023-05-28 00:16:21 +0900

    PlayerJump

--- Assets/02_Script/Player/Movement/PlayerJump.cs
+++ Assets/02_Script/Player/Movement/PlayerJump.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerJump : PlayerBehaviorRoot
 {
 
+    private void Jump()
+    {
 
+        if (groundScncer.isSencing) rigid.velocity += Vector3.up * valueSystem.jumpPower;
+
+    }
 
     public override void AddEvent()
     {
 
-
+        eventSystem.OnJumpKeyPressEvent += Jump;
 
     }
 
     public override void RemoveEvent()
     {
 
-
+        eventSystem.OnJumpKeyPressEvent -= Jump;
 
     }
 }
