0dc5e375115d5b02359a2da57071ac7ee0611e2a
dkdkdsa <bos94330@gmail.com>
2023-05-27 22:19:53 +0900

    PlayerCameraRotate

--- Assets/02_Script/Player/Movement/PlayerCameraRotate.cs
+++ Assets/02_Script/Player/Movement/PlayerCameraRotate.cs
@@ -2,9 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCameraRotate : MonoBehaviour
+public class PlayerCameraRotate : PlayerBehaviorRoot
 {
-    
+
+    private float xInput, yInput;
 
 
+
+    private void SetX(float v) { xInput = v; }
+    private void SetY(float v) { yInput = v; }
+
+    public override void AddEvent()
+    {
+
+        eventSystem.OnMouseXEvent += SetX;
+        eventSystem.OnMouseYEvent -= SetY;
+
+    }
+
+    public override void RemoveEvent()
+    {
+
+        eventSystem.OnMouseXEvent -= SetX;
+        eventSystem.OnMouseYEvent -= SetY;
+    }
 }
