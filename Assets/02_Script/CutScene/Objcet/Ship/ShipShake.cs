e49eaf63caab9edaa9281f997b188ac563af2524
dkdkdsa <bos94330@gmail.com>
2023-06-03 23:43:19 +0900

    SineAdd

--- Assets/02_Script/CutScene/Objcet/Ship/ShipShake.cs
+++ Assets/02_Script/CutScene/Objcet/Ship/ShipShake.cs
@@ -1,9 +1,12 @@
+using Class;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipShake : MonoBehaviour
 {
+
+    [SerializeField] private SineAdd sineAdd;
 
     private void Awake()
     {
