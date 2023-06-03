ecb556a80b9581e42e80317ada5158a62226dabc
dkdkdsa <bos94330@gmail.com>
2023-06-03 23:39:04 +0900

    SineAdd

--- Assets/02_Script/System/Class.cs
+++ Assets/02_Script/System/Class.cs
@@ -5,6 +5,45 @@ using UnityEngine;
 namespace Class
 {
 
+    [System.Serializable]
+    public class SineClass
+    {
 
+        [field:SerializeField] public float x { get; private set; }
+        [field: SerializeField] public float y { get; private set; }
+
+        public float GetSineGrahp()
+        {
+
+            return x * Mathf.Sin(y * Time.time);
+
+        }
+
+    }
+
+    [System.Serializable]
+    public class SineAdd
+    {
+
+        [SerializeField] private SineClass[] sineClasses = null;
+
+        public float GetSinValue()
+        {
+
+            float xVel = 0, sinVel = 0;
+
+            foreach (var s in sineClasses) 
+            {
+
+                sinVel += s.GetSineGrahp();
+                xVel += s.x;
+
+            }
+
+            return (sinVel/ (xVel * 2)) + 0.5f;
+
+        }
+
+    }
 
 }
