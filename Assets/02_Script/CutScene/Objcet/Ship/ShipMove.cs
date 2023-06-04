11ffc47b74f788f18ce26a1be7b5c9f6da3be09e
dkdkdsa <bos94330@gmail.com>
2023-06-04 22:04:27 +0900

    Map

--- Assets/02_Script/CutScene/Objcet/Ship/ShipMove.cs
+++ Assets/02_Script/CutScene/Objcet/Ship/ShipMove.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ShipMove : MonoBehaviour
 {
 
     [SerializeField] private float endZ;
     [SerializeField] private float moveSpd;
+    [SerializeField] private UnityEvent endEvt;
 
     private void FixedUpdate()
     {
         
+        if(transform.position.z >= endZ)
+        {
+
+            endEvt?.Invoke();
+            return;
+
+        }
+
         transform.Translate(transform.forward * Time.fixedDeltaTime * moveSpd);
 
     }
