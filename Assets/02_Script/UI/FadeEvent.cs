a7bbd06e429ed14a6646e022dd5e8f895aa7bd17
dkdkdsa <bos94330@gmail.com>
2023-06-04 22:43:00 +0900

    FadeEvent

--- Assets/02_Script/UI/FadeEvent.cs
+++ Assets/02_Script/UI/FadeEvent.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.Events;
 
 public class FadeEvent : MonoBehaviour
 {
+
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private UnityEvent fadeInEndEvt, fadeOutEndEvt;
 
     private Image baseImage;
 
@@ -18,7 +23,14 @@ public class FadeEvent : MonoBehaviour
     public void FadeIn()
     {
 
+        baseImage.DOFade(1, fadeDuration).OnComplete(() => fadeInEndEvt.Invoke());
 
+    }
+
+    public void FadeOut()
+    {
+
+        baseImage.DOFade(0, fadeDuration).OnComplete(() => fadeOutEndEvt.Invoke());
 
     }
 
