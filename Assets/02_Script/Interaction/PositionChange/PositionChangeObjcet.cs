879a70174559d833a9549bff7869d62a2ce9ba3b
dkdkdsa <bos94330@gmail.com>
2023-06-09 01:16:12 +0900

    PositionChangeObjcet

---
+++ Assets/02_Script/Interaction/PositionChange/PositionChangeObjcet.cs
@@ -0,0 +1,32 @@
+using Enums;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionChangeObjcet : InteractionObjectRoot
+{
+
+    private Transform playerTrm;
+
+    private void Awake()
+    {
+
+        playerTrm = GameObject.Find("Player").transform;
+
+    }
+
+    public override void OnCursorClickEvent(InteractionClickKey key)
+    {
+
+        
+
+    }
+
+    public override void OnCursorOnEvent()
+    {
+
+        //나중에 UI 추가 예정
+
+    }
+
+}
--- Assets/02_Script/Interaction/Root/InteractionObjectRoot.cs
+++ Assets/02_Script/Interaction/Root/InteractionObjectRoot.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Enums;
 
 public abstract class InteractionObjectRoot : MonoBehaviour
 {
 
+    public abstract void OnCursorClickEvent(InteractionClickKey key);
     public abstract void OnCursorOnEvent();
-    public abstract void OnCursorClickEvent();
 
 }
