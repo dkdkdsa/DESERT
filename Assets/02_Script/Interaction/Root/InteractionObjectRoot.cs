2bdc5d2d4fc32d743af9a1c3ba66a9e62007afa8
dkdkdsa <bos94330@gmail.com>
2023-06-09 00:26:36 +0900

    InteractionObjectRoot

--- Assets/02_Script/Interaction/Root/InteractionObjectRoot.cs
+++ Assets/02_Script/Interaction/Root/InteractionObjectRoot.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractionObjectRoot : MonoBehaviour
+public abstract class InteractionObjectRoot : MonoBehaviour
 {
 
-
+    public abstract void OnCursorOnEvent();
+    public abstract void OnCursorClickEvent();
 
 }
