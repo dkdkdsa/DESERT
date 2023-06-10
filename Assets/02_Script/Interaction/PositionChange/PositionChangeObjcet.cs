00aa8b149706e887b2508dcf9e04dfa1ce8606ea
dkdkdsa <bos94330@gmail.com>
2023-06-11 02:49:10 +0900

    PositionChangeObjcet

--- Assets/02_Script/Interaction/PositionChange/PositionChangeObjcet.cs
+++ Assets/02_Script/Interaction/PositionChange/PositionChangeObjcet.cs
@@ -7,7 +7,9 @@ public class PositionChangeObjcet : InteractionObjectRoot
 {
 
     [SerializeField] private float sencingRange = 5;
+
     private Transform playerTrm;
+    private bool isPosChanging = false;
 
     private void Awake()
     {
@@ -19,10 +21,10 @@ public class PositionChangeObjcet : InteractionObjectRoot
     public override void OnCursorClickEvent(InteractionClickKey key)
     {
 
-        if(key == InteractionClickKey.Left && Vector3.Distance(transform.position, playerTrm.position) <= sencingRange)
+        if(key == InteractionClickKey.Left && Vector3.Distance(transform.position, playerTrm.position) <= sencingRange && !isPosChanging)
         {
 
-
+            StartCoroutine(PosChangeCo());
 
         }
 
@@ -49,10 +51,41 @@ public class PositionChangeObjcet : InteractionObjectRoot
     private IEnumerator PosChangeCo()
     {
 
+        isPosChanging = true;
+
         yield return null;
 
+        float per = 0;
+
+        while (per < 1)
+        {
+
+            per += Time.deltaTime;
+            CameraManager.instance.SetFOV(
+                Mathf.Lerp(CameraManager.instance.GetFOV(), 50, per));
+            yield return null;
+
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        ChangePosition();
+
+        yield return new WaitForSeconds(0.01f);
+
+        while (per < 1)
+        {
+
+            per += Time.deltaTime * 5;
+            CameraManager.instance.SetFOV(
+                Mathf.Lerp(CameraManager.instance.GetFOV(), 60, per));
+            yield return null;
 
 
+
+        }
+
+        isPosChanging = false;
 
     }
 
