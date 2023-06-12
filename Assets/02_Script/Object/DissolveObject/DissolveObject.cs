0bb7aa5996d022798fd4416d481003c81fc134f1
dkdkdsa <bos94330@gmail.com>
2023-06-12 13:31:29 +0900

    DissolveObject

--- Assets/02_Script/Object/DissolveObject/DissolveObject.cs
+++ Assets/02_Script/Object/DissolveObject/DissolveObject.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class DissolveObject : MonoBehaviour
 {
 
-    [SerializeField] private GameObject dissolveManageObject;
     [SerializeField] private InteractionObjectRoot[] interactionObjects;
 
     private void Awake()
     {
-        
 
+        foreach (var interactionObject in interactionObjects)
+        {
+
+            interactionObject.enabled = false;
+
+        }
+
+    }
+
+    public void EnableObject()
+    {
+
+        foreach (var interactionObject in interactionObjects)
+        {
+
+            interactionObject.enabled = true;
+
+        }
 
     }
 
