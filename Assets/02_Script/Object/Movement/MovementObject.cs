22ecddb591eeb7e1a1863e117dc633f140278a5d
dkdkdsa <bos94330@gmail.com>
2023-06-13 17:19:17 +0900

    MovementEvent

--- Assets/02_Script/Object/Movement/MovementObject.cs
+++ Assets/02_Script/Object/Movement/MovementObject.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class MovementEvent : MonoBehaviour
 {
-    
 
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float speed;
+    [SerializeField] private float moveTime;
+
+    private float curTime;
+    private bool isMovementing = false;
+
+    private void FixedUpdate()
+    {
+
+        Movement();
+
+    }
+
+    private void Movement()
+    {
+
+        if (!isMovementing || curTime >= moveTime) return;
+
+        curTime += Time.fixedDeltaTime;
+
+        transform.Translate(direction * speed * Time.fixedDeltaTime);
+
+    }
+
+    public void StartMovement()
+    {
+
+        isMovementing = true;
+
+    }
 
 }
