9c0b168da7dc4e5bb7aafeea6094802a6c829479
dkdkdsa <bos94330@gmail.com>
2023-06-17 22:23:00 +0900

    Assets

---
+++ Assets/Amazing Assets/Advanced Dissolve/Example Scenes/Demo Scenes/Files/Scripts/UIChangeSize.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AmazingAssets.AdvancedDissolve.ExampleScripts
+{
+    public class UIChangeSize : MonoBehaviour
+    {
+        public AdvancedDissolveGeometricCutoutController geometricCutoutController;
+
+        public Light spotLight;
+        public float radius = 5;
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Plus) || Input.GetKey(KeyCode.KeypadPlus) || Input.GetKey(KeyCode.Equals))
+            {
+                radius += Time.deltaTime * 3;
+                radius = Mathf.Clamp(radius, 1, 20);
+            }
+
+            if (Input.GetKey(KeyCode.Minus) || Input.GetKey(KeyCode.KeypadMinus))
+            {
+                radius -= Time.deltaTime * 3;
+                radius = Mathf.Clamp(radius, 1, 20);
+            }
+
+
+            //Update mask object (first mask object only)
+            //Note, in demo scenes we have only one mask controller
+            switch (geometricCutoutController.type)
+            {
+                case AdvancedDissolveKeywords.CutoutGeometricType.Sphere:
+                    geometricCutoutController.target1Radius = radius;
+                    break;
+                case AdvancedDissolveKeywords.CutoutGeometricType.Cube:
+                    transform.localScale = Vector3.one * radius;
+                    break;
+                case AdvancedDissolveKeywords.CutoutGeometricType.Capsule:
+                    geometricCutoutController.target1Radius = radius;
+                    break;
+                case AdvancedDissolveKeywords.CutoutGeometricType.ConeSmooth:
+                    {
+                        spotLight.spotAngle = radius * 2;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Amazing Assets/Advanced Dissolve/Example Scenes/Demo Scenes/Files/Scripts/UIInput.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace AmazingAssets.AdvancedDissolve.ExampleScripts
+{
+    public class UIInput : MonoBehaviour
+    {
+        public AdvancedDissolveGeometricCutoutController geometricCutoutController;
+
+        public Text fpsText;
+        float deltaTime = 0.0f;
+        bool displayFPS;
+
+        public GameObject menu;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //FPS
+            if (Input.GetKeyDown(KeyCode.F))
+                displayFPS = !displayFPS;
+
+            //VSync
+            if (Input.GetKeyDown(KeyCode.V))
+                QualitySettings.vSyncCount = (QualitySettings.vSyncCount == 0) ? 1 : 0;
+
+            //Noise
+            if (Input.GetKeyDown(KeyCode.N))
+                geometricCutoutController.noise = (geometricCutoutController.noise > 0) ? 0 : 1;
+
+            //Invert
+            if (Input.GetKeyDown(KeyCode.I))
+                geometricCutoutController.invert = !geometricCutoutController.invert;
+
+            //Show/Hide Menu
+            if (Input.GetKeyDown(KeyCode.H))
+                menu.SetActive(!menu.activeSelf);
+
+
+            UpdateFPS();
+        }
+
+
+        void UpdateFPS()
+        {
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+
+            if (displayFPS)
+            {
+                float fps = 1.0f / deltaTime;
+                fpsText.text = (int)fps + " fps";
+            }
+            else
+                fpsText.text = string.Empty;
+        }
+    }
+}
