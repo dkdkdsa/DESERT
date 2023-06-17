e8af304d8ced6df52e110d56e4c9f2c08182c931
dkdkdsa <bos94330@gmail.com>
2023-06-17 22:31:30 +0900

    FairyMovement

--- Assets/02_Script/Object/Fairy/FairyMovement.cs
+++ Assets/02_Script/Object/Fairy/FairyMovement.cs
@@ -1,3 +1,4 @@
+using Enums;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,25 @@ public class FairyMovement : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
 
+    private FairyState state;
+    private Vector3 targetPos;
+
+    private void Awake()
+    {
+        
+        targetPos = transform.position;
+
+    }
 
     private void Update()
     {
         
+        if(state == FairyState.Idle)
+        {
 
+            
+
+        }
 
     }
 
