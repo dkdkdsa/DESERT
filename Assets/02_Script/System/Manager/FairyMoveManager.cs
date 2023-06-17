447b315f6c019017785ca65888082efb2fe7a1b2
dkdkdsa <bos94330@gmail.com>
2023-06-17 22:46:27 +0900

    FairyMoveManager

--- Assets/02_Script/System/Manager/FairyMoveManager.cs
+++ Assets/02_Script/System/Manager/FairyMoveManager.cs
@@ -1,10 +1,11 @@
+using Class;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FairyMoveManager : MonoBehaviour
 {
-    
 
+    [SerializeField] private List<FairyMoveClass> moves = new List<FairyMoveClass>();
 
 }
