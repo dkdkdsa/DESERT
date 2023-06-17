c9d56e1a71f95f3a1e124faa5d8317e75c548b1e
dkdkdsa <bos94330@gmail.com>
2023-06-17 22:48:50 +0900

    FairyMoveManager::SetMove

--- Assets/02_Script/System/Manager/FairyMoveManager.cs
+++ Assets/02_Script/System/Manager/FairyMoveManager.cs
@@ -8,4 +8,22 @@ public class FairyMoveManager : MonoBehaviour
 
     [SerializeField] private List<FairyMoveClass> moves = new List<FairyMoveClass>();
 
+    private FairyMovement fairy;
+
+    private void Awake()
+    {
+        
+        fairy = FindObjectOfType<FairyMovement>();
+
+    }
+
+    public void SetMove(string key)
+    {
+
+        var obj = moves.Find(x => x.key == key);
+
+        fairy.SetMoev(obj.targetObj.transform.position, obj.speed);
+
+    }
+
 }
