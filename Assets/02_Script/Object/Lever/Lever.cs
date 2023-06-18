8f17601c6759e9f899a778f5c84c36012844c0ce
dkdkdsa <bos94330@gmail.com>
2023-06-18 15:52:59 +0900

    Material

--- Assets/02_Script/Object/Lever/Lever.cs
+++ Assets/02_Script/Object/Lever/Lever.cs
@@ -1,10 +1,32 @@
+using Enums;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lever : MonoBehaviour
+public class Lever : InteractionObjectRoot
 {
-    
+
+    private LeverAnimator animator;
+    private bool isInteraction;
+
+    private void Awake()
+    {
+        
+        animator = GetComponentInChildren<LeverAnimator>();
+
+    }
+
+    public override void OnCursorClickEvent(InteractionClickKey key)
+    {
 
 
+
+    }
+
+    public override void OnCursorOnEvent()
+    {
+
+
+
+    }
 }
