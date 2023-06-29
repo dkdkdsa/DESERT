0c0aeeb0253103f2ca8d17e37f92eb0609c8eb1b
dkdkdsa <bos94330@gmail.com>
2023-06-29 23:20:33 +0900

    PlayerDarkMode

--- Assets/02_Script/Player/Mode/PlayerDarkMode.cs
+++ Assets/02_Script/Player/Mode/PlayerDarkMode.cs
@@ -10,6 +10,20 @@ public class PlayerDarkMode : PlayerBehaviorRoot
 
         if (!valueSystem.darkMode) return;
 
+        var horVec = Input.GetAxisRaw("Horizontal");
+        var vetVec = Input.GetAxisRaw("Vertiacl");
+
+        float rootVec = Mathf.Abs(horVec) + Mathf.Abs(vetVec);
+
+        rootVec = Mathf.Clamp(rootVec, 0, 1);
+
+        if(rootVec > 0)
+        {
+
+            valueSystem.darkGauge -= Time.deltaTime * 0.01f;
+
+        }
+
     }
 
     public override void AddEvent()
