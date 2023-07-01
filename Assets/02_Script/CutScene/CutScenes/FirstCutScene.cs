b148dc661918e32b60c0208c76e5a94957643608
dkdkdsa <bos94330@gmail.com>
2023-07-02 02:18:47 +0900

    FirstCutScene

--- Assets/02_Script/CutScene/CutScenes/FirstCutScene.cs
+++ Assets/02_Script/CutScene/CutScenes/FirstCutScene.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FirstCutScene : CutSceneRoot
 {
+
+    [SerializeField] private Transform camObject;
+
     public override void StartCutScene()
     {
 
@@ -14,7 +17,21 @@ public class FirstCutScene : CutSceneRoot
     private IEnumerator CutSceneCo()
     {
 
+        yield return null;
+        //yield return new WaitForSeconds(5f);
 
+        float per = 0;
+
+        while(per < 1)
+        {
+
+            per += Time.deltaTime * 0.5f;
+
+            var vec = new Vector3(0, Mathf.Lerp(-0.4f, 1, per), 0);
+
+            camObject.transform.position = vec;
+
+        }
 
     }
 
