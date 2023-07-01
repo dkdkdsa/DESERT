9a25ba914fc5c346a15a0de8d6e2b1b1d3db9990
dkdkdsa <bos94330@gmail.com>
2023-07-02 02:11:26 +0900

    CutSceneRoot

--- Assets/02_Script/CutScene/Root/CutSceneRoot.cs
+++ Assets/02_Script/CutScene/Root/CutSceneRoot.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CutSceneRoot : MonoBehaviour
+public abstract class CutSceneRoot : MonoBehaviour
 {
-    
 
+    [SerializeField] private UnityEvent cutSceneEndEvent;
+
+    public abstract void StartCutScene();
 
 }
