15b4f78686097b88dd9b06a426518036978a2757
dkdkdsa <bos94330@gmail.com>
2023-07-03 09:38:40 +0900

    FairyLine

--- Assets/02_Script/Object/FairyLine/FairyLine.cs
+++ Assets/02_Script/Object/FairyLine/FairyLine.cs
@@ -1,10 +1,23 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FairyLine : MonoBehaviour
 {
-    
 
+    [SerializeField] private Vector3 start, startControll, end, endControll;
+    [SerializeField] private Vector3[] arr;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private int count;
+
+    public void SettingArray()
+    {
+
+        lineRenderer.positionCount = count;
+        arr = DOCurve.CubicBezier.GetSegmentPointCloud(start, startControll, end, endControll, count);
+        lineRenderer.SetPositions(arr);
+
+    }
 
 }
