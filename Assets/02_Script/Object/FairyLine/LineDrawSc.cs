d74a320493ab0db4e04fbf8c25bb1df0157d7d7f
dkdkdsa <bos94330@gmail.com>
2023-07-03 09:27:19 +0900

    LineDrawSc

--- Assets/02_Script/Object/FairyLine/LineDrawSc.cs
+++ Assets/02_Script/Object/FairyLine/LineDrawSc.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class LineDrawSc : MonoBehaviour
 {
 
+    [HideInInspector] public Vector3[] renderArr;
+
+    public void SetArr(Vector3[] renderArr)
+    {
+
+        this.renderArr = renderArr;
+
+    }
+
 #if UNITY_EDITOR
 
     private void OnDrawGizmosSelected()
