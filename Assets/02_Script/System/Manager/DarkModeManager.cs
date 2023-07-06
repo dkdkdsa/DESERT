542f71f1ee0a12a997ae779e9241e94a8774582a
dkdkdsa <bos94330@gmail.com>
2023-07-07 01:10:33 +0900

    DarkModeManager

---
+++ Assets/02_Script/System/Manager/DarkModeManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkModeManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] darkModeObjcetArr;
+
+    public static DarkModeManager instance { get; private set; }
+
+    private void Awake()
+    {
+        
+        foreach(var item in darkModeObjcetArr)
+        {
+
+            item.SetActive(false);
+
+        }
+
+        instance = this;
+
+    }
+
+    public void SetMode(bool enable)
+    {
+
+        if (enable)
+        {
+
+            foreach (var item in darkModeObjcetArr)
+            {
+
+                item.SetActive(true);
+
+            }
+
+        }
+        else
+        {
+
+            foreach (var item in darkModeObjcetArr)
+            {
+
+                item.SetActive(false);
+
+            }
+
+        }
+
+    }
+
+}
